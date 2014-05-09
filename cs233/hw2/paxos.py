aa276e7f9ec71526d7ef65baaa2e8f2ba376aa3e
Hall Liu <hallliu000@gmail.com>
2014-05-09 04:08:41 -0500

    c2

--- cs233/hw2/paxos.py
+++ cs233/hw2/paxos.py
@@ -26,7 +26,7 @@ class Event():
         # Check for proposals and run them if present
         if self.prop is not None and self.val is not None:
             msg = Msg(None, computers[self.prop], 'PROPOSE', {'val': self.val})
-            computers[self.prop].recv_message(msg)
+            computers[self.prop].recv_message(msg, self.t)
             return True
 
         return False
@@ -34,18 +34,74 @@ class Event():
 
 class Computer():
     msg_dispatch = {
-            'PROPOSE': Computer.propose
+            'PROPOSE': Computer.propose,
+            'PREPARE': Computer.prepare,
+            'PROMISE': Computer.promise,
+            'ACCEPT': Computer.accept,
+            'ACCEPTED': Computer.accepted,
+            'REJECTED': Computer.rejected
     }
 
     def __init__(self, cid, **kwargs):
         self.cid = cid
         self.other_computers = kwargs.pop('computers')
-        self.mq = kwargs.pop('mq')
+        acceptor_count = len(self.other_computers[1])
+        self.majority_count = acceptor_count / 2 + 1
+
         self.failed = False
-        self.role = cid[0]
-        self.misc_data = kwargs
+        self.highest_promised = 0
+        self.highest_accepted = None
+        self.__dict__.update(kwargs)
+
+        self.state = ('IDLE', {})
     
-    def recv_message(self, msg):
+    def recv_message(self, msg, t):
+        msg_dispatch[msg.mtype](self, msg, t)
+
+    def propose(self, msg, t):
+        prop_ctr = self.prop_ctr
+        this_prop_no = prop_ctr[0]
+        prop_ctr[0] += 1
+
+        msg_data = {'prop_no': this_prop_no}
+        for c in self.other_computers[1]:
+            new_msg = Msg(self, c, 'PREPARE', msg_data)
+            self.mq.append(new_msg)
+
+        print '{0}: {1} -> {2} PROPOSE v={3}'.format(t, '  ', computers[self.prop].cid, msg.data['val'])
+
+        self.state = ('PROMISE_WAIT', {'n_proms': 0, 'n_rejs': 0, 'prop_val': msg.data['val']})
+
+    def prepare(self, msg, t):
+        print '{0}: {1} -> {2} PREPARE n={3}'.format(t, msg.src.cid, self.cid, msg.data['prop_no'])
+        pn = msg.data['prop_no']
+        if pn < self.highest_promised:
+            reject_msg = Msg(self, msg.src, 'REJECTED', {'prop_no': pn})
+            self.mq.append(reject_msg)
+            return
+        
+        prom_msg = Msg(self, msg.src, 'PROMISE', {'prop_no': pn, 'prior': self.highest_accepted})
+        self.mq.append(prom_msg)
+        return
+
+    def promise(self, msg, t):
+        out_str = '{0}: {1} -> {2} PROMISE n={3}'.format(t, msg.src.cid, self.cid, msg.data['prop_no'])
+        if msg.data['prior'] is not None:
+            print out_str + ' (Prior: n={0}, v={1})'.format(*msg.data['prior'])
+        else:
+            print out_str + ' (Prior: None)'
+
+
+        if self.state[0] != 'PROMISE_WAIT':
+            raise Exception()
+
+        self.state[1]['n_proms'] += 1
+        if self.state[1]['n_proms'] >= self.majority_count:
+            for c in self.other_computers[1]:
+
+
+
+
 
 
 
@@ -95,16 +151,19 @@ def extract_message(net_queue):
 
 def run_paxos(n_prop, n_acc, tmax, events):
     net_queue = []
-    computers = {}
+    proposers = {}
+    acceptors = {}
+    prop_number = [1]
     for i in range(1, n_prop + 1):
         cid = 'P' + str(i)
-        computers[cid] = Computer(cid, computers=computers, mq=net_queue, n_acc=n_acc)
+        proposers[cid] = Computer(cid, computers=(proposers, acceptors), mq=net_queue, prop_ctr = prop_number)
     for i in range(1, n_acc + 1):
         cid = 'A' + str(i)
-        computers[cid] = Computer(cid, computers=computers, mq=net_queue)
+        acceptors[cid] = Computer(cid, computers=(proposers, acceptors), mq=net_queue)
 
-    prop_number = [1]
     work_done = False
+    computers = proposers.copy()
+    computers.update(acceptors)
 
     for tick in range(tmax):
         if len(events) == 0 and len(net_queue) == 0:
@@ -113,7 +172,7 @@ def run_paxos(n_prop, n_acc, tmax, events):
         # See if there's an event to be processed
         e = events[0]
         if e.t == tick:
-            work_done = e.run(net_queue, computers, prop_number)
+            work_done = e.run(net_queue, computers)
         
         # If event contained a proposal, then we do no more work this tick.
         if work_done:
@@ -124,4 +183,4 @@ def run_paxos(n_prop, n_acc, tmax, events):
             print str(tick) + ':'
             continue
 
-        msg.dst.recv_message(msg)
+        msg.dst.recv_message(msg, t)
